6f59a540e4c39425db8be21f3e941de8b060d782
KeenanScully <164510405+KeenanScully@users.noreply.github.com>
2026-09-22 18:49:59 +0200

    Add files via upload

---
+++ Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudyFlowApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        //Simple endpoint used to confirm the StudyFlow API is running
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                message = "StudyFlow API is running!",
+                status = "success"
+            });
+        }
+
+        //Used by the hosting platform to check if API is responding.
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            return Ok(new
+            {
+                status = "health"
+            });
+        }
+    }
+}
